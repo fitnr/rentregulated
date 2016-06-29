395a40cde3ac6142b7ae68986800ac1bb02413ca
fitnr <fitnr@fakeisthenewreal>
2016-06-29 00:49:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+New York Rent Regulated Buildings 
+---------------------------------
+
+In New York, many privately-owned apartments are subject to limits on the rent landlords can ask. The vast majority of these apartments are called *rent-stabilized*. An older program, *rent-control*, has even stronger limits on rent increases and evictions.
+
+Owners of buildings with rent-controlled and rent-stabilized apartments must register them with the state [Division of Housing and Community Renewal](http://www.nyshcr.org). The registry DHCR keeps also includes information about housing subsidy programs that apply to these buildings.
+
+You can [search the DHCR registry](https://apps.hcr.ny.gov/BuildingSearch/), but it's difficult to get complete data. This data set was compiled using a computer program to methodically request all of data from the DCHR site in mid-2016.
+
+The vast majority of properties are in the five boroughs, most of the rest are also downstate.
+
+DCHR has provided [basic information](https://apps.hcr.ny.gov/buildingsearch/popup.aspx) about the fields in the data set.
+
+## Caveats
+* Some addresses are given as ranges
+* Some properties have "additional addressses". The significance of these addresses is unexplained. This data is available on the DHCR site, but requires an additional step to scrape, so only the note that an additional address exists is included.
+* The `status` field includes a a space-separated of status that apply to a building. One or more status may be present. The exact wording of each status may differ slightly, so be careful with filtering. The statuses, with links to the best explanation available:
+** [MULTIPLE DWELLING A](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#DwellingA)
+** [MULTIPLE DWELLING B](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#DwellingB)
+** [HOTEL](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#Hotel)
+** [SINGLE ROOM OCCUPANCY](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#SingleRoom)
+** [GARDEN APARTMENT COMPLEX](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#Garden)
+** [NON-EVICT COOP/CONDO](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#NonEvict) (may include an effective date in parenthese)
+** [EVICT COOP/CONDO](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#Evict) (may include an effective date in parenthese)
+** [COOP/CONDO PLAN FILED](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#Coop) (may include a file date in parentheses)
+** [J-51](http://www1.nyc.gov/site/hpd/developers/tax-incentives-j51.page)
+** [421-A](http://www1.nyc.gov/site/hpd/developers/tax-incentives-421a.page)
+** [ARTICLE 11 OF PHFL](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#Article11)
+** [ARTICLES 14 & 15 OF PHFL](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#Article14and15)
+** Section 608
+** [Not Indicated by Owner](https://apps.hcr.ny.gov/buildingsearch/popup.aspx#NotIndicated)
+** RENT CONTROLLED APTS MAY EXIST
---
+++ scrape.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3.5
+import sys
+import re
+import time
+import requests
+from bs4 import BeautifulSoup
+
+LIB = 'lxml'
+ENDPOINT = 'https://apps.hcr.ny.gov/buildingsearch/'
+
+agent = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/51.0.2704.103 Safari/537.36"
+)
+
+ajax_head = {
+    'X-MicrosoftAjax': 'Delta=true',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+def dumb_params(soup):
+    try:
+        return {
+            '__VIEWSTATE': soup.find(attrs={'name': '__VIEWSTATE'}).attrs['value'],
+            '__EVENTVALIDATION': soup.find(attrs={'name': '__EVENTVALIDATION'}).attrs['value'],
+            '__VIEWSTATEGENERATOR': soup.find(attrs={'name': '__VIEWSTATEGENERATOR'}).attrs['value'],
+            '__VIEWSTATEENCRYPTED': soup.find(attrs={'name': '__VIEWSTATEENCRYPTED'}).attrs['value'],
+            '__EVENTARGUMENT': '',
+            '__LASTFOCUS': '',
+        }
+    except AttributeError as err:
+        # import pdb; pdb.set_trace()
+        raise err
+
+
+def construct_soup(text):
+    fields = 'VIEWSTATE', 'EVENTVALIDATION', 'VIEWSTATEGENERATOR', 'VIEWSTATEENCRYPTED'
+    vals = dict()
+
+    for f in fields:
+        match = re.search(r'__' + f + r'\|([^|]*)\|', text)
+        vals[f] = match.groups()[0] if match else ''
+
+    document = '''{} <input value="{VIEWSTATE}" name="__VIEWSTATE" />
+        <input value="{EVENTVALIDATION}" name="__EVENTVALIDATION" />
+        <input value="{VIEWSTATEGENERATOR}" name="__VIEWSTATEGENERATOR" />
+        <input value="{VIEWSTATEENCRYPTED}" name="__VIEWSTATEENCRYPTED" />'''
+
+    html = document.format(text, **vals)
+    return BeautifulSoup(html, LIB)
+
+
+def clean(text):
+    text = re.sub(r'\s+', ' ', text.strip(' \r\n'))
+    if ',' in text:
+        return '"' + text + '"'
+    else:
+        return text
+
+
+def writerows(f, soup):
+    for tr in soup.find(attrs={'class': 'grid'}).find_all('tr'):
+        if tr.td is None or tr.td.attrs.get('colspan') == 7:
+            continue
+
+        if 'Displaying buildings ' in str(tr):
+            continue
+
+        try:
+            row = [clean(td.text) for td in tr.find_all('td')]
+        except TypeError:
+            import pdb
+            pdb.set_trace()
+
+        f.write(','.join(row) + '\n')
+
+
+def prepare(sesh):
+    sesh.headers.update({
+        'User-Agent': agent,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'Origin': 'https://apps.hcr.ny.gov',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Referer': 'https://apps.hcr.ny.gov/buildingsearch/default.aspx',
+    })
+
+    # request page, get session cookie
+    print('starting session', file=sys.stderr)
+    r = sesh.get(ENDPOINT)
+    soup = BeautifulSoup(r.text, LIB)
+    sesh.headers.update({
+        'Cookie': 'ASP.NET_SessionId=' + sesh.cookies['ASP.NET_SessionId']
+    })
+    time.sleep(0.025)
+
+    # poke zip code button
+    param = dumb_params(soup)
+    param.update({
+        '__EVENTTARGET': 'ctl00$ContentPlaceHolder1$zipCodeSearchLinkButton',
+        'ctl00$ContentPlaceHolder1$countyDropDown': '',
+    })
+    time.sleep(0.025)
+
+    print('poking zip code button', file=sys.stderr)
+    r = sesh.post(ENDPOINT, data=param)
+    return BeautifulSoup(r.text, LIB)
+
+
+def main(county, zipcode):
+    basic = {
+        'ctl00$ContentPlaceHolder1$countyListDropDown': county,
+        'ctl00$ContentPlaceHolder1$zipCodesDropDown': zipcode,
+        '__EVENTTARGET': '',
+    }
+
+    with requests.Session() as sesh:
+        soup = prepare(sesh)
+
+        # select county from dropdown list
+        params = {
+            'ctl00$ContentPlaceHolder1$countyListDropDown': county,
+            'ctl00$ContentPlaceHolder1$zipCodesDropDown': '',
+            '__EVENTTARGET': 'ctl00$ContentPlaceHolder1$countyListDropDown',
+            '__ASYNCPOST': 'true',
+            'ctl00$ContentPlaceHolder1$ScriptManager1': (
+                    'ctl00$ContentPlaceHolder1$ScriptManager1|'
+                    'ctl00$ContentPlaceHolder1$countyListDropDown'
+            ),
+        }
+        params.update(dumb_params(soup))
+        print('selecting county:', county, file=sys.stderr)
+        breq = sesh.post(ENDPOINT, data=params, headers=ajax_head)
+        time.sleep(0.025)
+
+        # decode parameters out of nasty nonsense
+        # easiest way is to make another soup object
+        soup = construct_soup(breq.text)
+
+        params = {
+            'ctl00$ContentPlaceHolder1$submitZipCodeButton': 'Submit',
+        }
+        params.update(basic)
+        params.update(dumb_params(soup))
+
+        req = sesh.post(ENDPOINT, data=params)
+
+        if len(req.history) > 1:
+            # import pdb; pdb.set_trace()
+            raise RuntimeError("too many histories")
+
+        if re.search('0 results found', req.text):
+            return
+
+        next_button = re.search(r'value="Next"', req.text)
+        soup = BeautifulSoup(req.text, LIB)
+
+        writerows(sys.stdout, soup)
+
+        while next_button:
+            print('  next 50...', file=sys.stderr)
+            params = {
+                "ctl00$ContentPlaceHolder1$ScriptManager1": (
+                    "ctl00$ContentPlaceHolder1$gridUpdatePanel|"
+                    'ctl00$ContentPlaceHolder1$buildingsGridView$ctl54$btnNext'
+                ),
+                'ctl00$ContentPlaceHolder1$buildingsGridView$ctl54$btnNext': "Next",
+                '__ASYNCPOST': 'true',
+            }
+            params.update(basic)
+            params.update(dumb_params(soup))
+
+            req = sesh.post(ENDPOINT, data=params, headers=ajax_head)
+            soup = construct_soup(req.text)
+
+            try:
+                writerows(sys.stdout, soup)
+            except AttributeError as e:
+                print(e, file=sys.stderr)
+                print(params.keys(), file=sys.stderr)
+                raise e
+                # import pdb; pdb.set_trace()
+
+            next_button = re.search(r'value="Next"', req.text)
+
+            time.sleep(0.025)
+
+if __name__ == '__main__':
+    c, z = sys.argv[1], sys.argv[2]
+    c.replace('NEWYORK', 'NEW YORK')
+    main(c, z)
